f9e8fd5ce4948ed0e1cad99cde933336f72fa84e
kirrito2105 <83820700+kirrito2105@users.noreply.github.com>
2025-07-02 15:43:40 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -27,7 +27,7 @@ wire reset = ~rst_n;
     wire _unuse = &{ena, uio_in, 1'b0};
     always  @(posedge clk or posedge reset) begin
         if (reset) begin
-            uo_out = 0;
+            uo_out <= 0;
         end else begin 
             uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
             uo_out[7:4] <= 0;
