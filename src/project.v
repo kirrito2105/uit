8109c395acc736315789f086d186a6f0a90d38ca
kirrito2105 <83820700+kirrito2105@users.noreply.github.com>
2025-07-02 15:29:12 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -9,7 +9,7 @@ module tt_um_asiclab_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
+    output reg [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -22,6 +22,16 @@ module tt_um_asiclab_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  reg _unused = &{ena, clk, rst_n, 1'b0};
-
+  wire _unused = &{ena, clk, rst_n, 1'b0};
+wire reset = ~rst_n;
+    assign uio_out = 0;
+    assign uio_oe =0;
+    wire _unuse = &{ena, uio_in, 1'b0};
+    always  @(posedge clk or posedge reset) begin
+        if (reset) begin
+            ou_out = 0;
+        end else begin 
+            ou_out[3:0] <= ui_in[7:4] + ui_in[3:0];
+            ou_out[7:4] <= 0;
+        end 
 endmodule
